728687d19caf79277445eb36cc24cf5551bebf04
Maslov <dmaslov@neoflex.ru>
2020-09-09 15:27:18 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import {Provider} from 'react-redux'
 import reduxThunk from 'redux-thunk'
 import rootReducer from './redux/rootReducer'
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
 
 const loggerMiddleware = store => next => action => {
     const result = next(action)
@@ -12,10 +19,10 @@ const loggerMiddleware = store => next => action => {
     return result
 }
 
-const store = createStore(rootReducer, applyMiddleware(
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(
     loggerMiddleware,
     reduxThunk
-))
+)))
 
 ReactDOM.render(
     <Provider store={store}>
