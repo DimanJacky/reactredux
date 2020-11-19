2f460f1eeb5593fe2de6b523d71f3bba929af1cd
dmaslov <dmaslov@neoflex.ru>
2020-11-19 16:46:18 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
 import {add, addNumber, asyncAdd} from "./redux/actions/actions";
+import {bindActionCreators} from "redux";
 
 class App extends Component {
   render() {
     return (
         <div className="App">
           <p>{this.props.counter}</p>
-          <button onClick={this.props.onAdd}>Добавить 1</button>
-          <button onClick={this.props.onAsyncAdd}>Добавить 1 через секунду</button>
-          <button onClick={() => this.props.onAddNumber(10)}>Добавить 10</button>
+          <button onClick={this.props.add}>Добавить 1</button>
+          <button onClick={this.props.asyncAdd}>Добавить 1 через секунду</button>
+          <button onClick={() => this.props.addNumber(10)}>Добавить 10</button>
         </div>
     )
   }
@@ -19,11 +20,12 @@ function mapStateToProps(state) {
     counter: state.counter.counter
   }
 }
-function mapDispatchToProps(dispatch) {
-  return {
-    onAdd: () => dispatch(add()),
-    onAddNumber: number => dispatch(addNumber(number)),
-    onAsyncAdd: () => dispatch(asyncAdd())
-  }
-}
+const mapDispatchToProps = (dispatch) => bindActionCreators(
+  {
+      add,
+      addNumber,
+      asyncAdd
+  },
+    dispatch
+);
 export default connect(mapStateToProps, mapDispatchToProps)(App)
